92e1e83aa86a0fc2ddf0a46fead954514b866b28
CodeExplorer <emula@live.fr>
2020-02-22 19:03:32 +0100

    *7

--- Front/src/components/social/Chat.js
+++ Front/src/components/social/Chat.js
@@ -19,6 +19,9 @@ class Chat extends Component {
                                 <li className="tab col s3"><a className="active" href="#test2">Test 2</a></li>
                                 <li className="tab col s3"><a href="#test3">Test 3</a></li>
                                 <li className="tab col s3"><a href="#test4">Test 4</a></li>
+                                <li className="tab col s3"><a href="#test5">Test 5</a></li>
+                                <li className="tab col s3"><a href="#test6">Test 6</a></li>
+                                <li className="tab col s3"><a href="#test7">Test 7</a></li>
                             </ul>
                         </div>
                         <div id="test1" className="col s12">Test 1</div>
