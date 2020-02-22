8c78d1d4b6d09d5b63500c12e9a99fa878f3426b
CodeExplorer <emula@live.fr>
2020-02-22 15:29:41 +0100

    Update - rottreducer repaired

--- Front/src/store/reducers/rootReducer.js
+++ Front/src/store/reducers/rootReducer.js
@@ -72,11 +72,11 @@ const rootReducer = (state = initState, action) => {
     else if (action.type === "NOTIF_ADD") {
         let notif_array = state.notifs;
         if (notif_array.length == 15) {
-            notif_array = notif.slice(1);
+            notif_array = notif_array.slice(1);
         }
         return {
             ...state,
-            notifs : [notif_array, action.payload.notif]
+            notifs : [...notif_array, action.payload.notif]
         }
     }
     return state;
