675f22c2e3264135fcbe2eedc0b78935eaab07c4
CodeExplorer <emula@live.fr>
2020-02-22 19:20:09 +0100

    props

--- Front/src/components/social/Chat.js
+++ Front/src/components/social/Chat.js
@@ -28,7 +28,7 @@ class Chat extends Component {
 
     componentDidMount() {
         M.Tabs.init(this.Tabs);
-        let rooms = get_rooms();
+        let rooms = get_rooms(this.props);
         console.log(rooms);
     }
 
