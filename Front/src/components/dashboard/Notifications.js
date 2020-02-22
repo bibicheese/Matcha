80af99ed5f93fe3886da5eb44fa2f7fc1685bb12
CodeExplorer <emula@live.fr>
2020-02-22 16:33:45 +0100

    REGIME

--- Front/src/components/dashboard/Notifications.js
+++ Front/src/components/dashboard/Notifications.js
@@ -28,8 +28,7 @@ class Notifications extends Component {
                         const status = n.readen ? <i className='fas fa-check green-text'></i> : <i className='fas fa-question'></i>;
                         return <div className="card" key={n.id}>
                             <div className="notif-card">
-                                <div className="card-title">n-title</div>
-                                <div className="card-content">n-content</div>
+                                <div className="card-content">{ n.msg }</div>
                                 <div className="notif-status">{ status }</div>
                             </div>
                         </div>
