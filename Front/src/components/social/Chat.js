e5d24bdc32015db3f440adc85aef77bb1f7ae6ba
CodeExplorer <emula@live.fr>
2020-02-22 19:02:59 +0100

    test3

--- Front/src/components/social/Chat.js
+++ Front/src/components/social/Chat.js
@@ -17,7 +17,7 @@ class Chat extends Component {
                             <ul className="tabs" ref={Tabs => {this.Tabs = Tabs;}}>
                                 <li className="tab col s3"><a href="#test1">Test 1</a></li>
                                 <li className="tab col s3"><a className="active" href="#test2">Test 2</a></li>
-                                <li className="tab col s3 disabled"><a href="#test3">Disabled Tab</a></li>
+                                <li className="tab col s3"><a href="#test3">Test 3</a></li>
                                 <li className="tab col s3"><a href="#test4">Test 4</a></li>
                             </ul>
                         </div>
