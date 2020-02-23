8ffd464680ee5cf76737248c93ad09b65e46245c
CodeExplorer <emula@live.fr>
2020-02-23 19:26:37 +0100

    :sob:

--- Front/src/components/profile/ProfileEdit.js
+++ Front/src/components/profile/ProfileEdit.js
@@ -408,7 +408,7 @@ export class ProfileEdit extends Component {
                 <div className="pictures-ui">
                     {this.state.images.map((image, index) => {
                         return (// eslint-disable-next-line
-                            <div className="btn red picture_ui" key={image + "-index:" + index} onClick={(e) => { this.deleteImage(e, image, index); }}> {index} <i className="fas fa-times white-text"></i></div>
+                            <div className="btn red picture_ui" key={image + "-index:" + index} onClick={(e) => this.deleteImage(e, image, index)}> {index} <i className="fas fa-times white-text"></i></div>
                             //<a key={index} className="carousel-item images"><img src={"http://localhost:8080/" + image['link']} alt="Some stuff"/></a>
                         )
                     })}
