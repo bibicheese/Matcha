e34ed6452d9f96321d2bc9706b548a34275d7963
CodeExplorer <emula@live.fr>
2020-02-23 11:53:47 +0100

    Update stuff

--- Front/src/components/social/Message.js
+++ Front/src/components/social/Message.js
@@ -14,9 +14,8 @@ class Message extends Component {
 
     render() {
         const classOfChip = this.state.sender === "Announcer" ? "announcer" : this.state.uid === this.state.sender ? "own" : "foreign";
-        const classOfWrapper = this.state.sender === "Announcer" ? "message-wrapper announcer-wrapper" : "message-wrapper";
         return (
-            <div className={ classOfWrapper } >
+            <div className="message-wrapper">
                 <div className={ "chip message " + classOfChip }>
                     {this.state.content}
                 </div>
